99fb0a86b551ccb0ddbf6f4469b79b824d799726
igor <bujhm1313@gmail.com>
2017-11-18 21:20:15 +0200

    [IMP] Add coordinates calculation for canvas

--- flask_server/static/js/find_face.js
+++ flask_server/static/js/find_face.js
@@ -1,6 +1,21 @@
 window.onload = function () {
     var c=document.getElementById("face_square");
     var ctx=c.getContext("2d");
-    ctx.rect(20,20,150,100);
+
+    // coordinates comes from the outer js
+    var coors = coordinates;
+    c.height = coors[0][2] + coors[0][0];
+    c.width = coors[0][3] + coors[0][1];
+    // ctx.rect(coors[0][0],coors[0][0],coors[0][2]-coors[0][0],coors[0][3]-coors[0][1]);
+
+    var x = coors[0][3];
+    var y = coors[0][0];
+
+    var height = coors[0][2] - coors[0][0];
+    var width = coors[0][1] - coors[0][3];
+    ctx.rect(x, y, width, height);
+
+    ctx.rect(x, y, width, height);
     ctx.stroke();
 };
+
