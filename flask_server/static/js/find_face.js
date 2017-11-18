36bc9796d6005510226a682193a6b97eefddcbb7
igor <bujhm1313@gmail.com>
2017-11-18 20:30:04 +0200

    [IMP] Add styles

--- flask_server/static/js/find_face.js
+++ flask_server/static/js/find_face.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    var c=document.getElementById("coordinates");
+    var c=document.getElementById("face_square");
     var ctx=c.getContext("2d");
     ctx.rect(20,20,150,100);
     ctx.stroke();
