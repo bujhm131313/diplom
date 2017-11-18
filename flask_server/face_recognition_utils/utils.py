111ea2aa8afe56550184eda35e64e848945d2144
igor <bujhm1313@gmail.com>
2017-11-18 22:42:04 +0200

    [REM] Remove face finding from client server

--- flask_server/face_recognition_utils/utils.py
+++
@@ -1,8 +0,0 @@
-import face_recognition
-
-
-def get_face_coordinates(img):
-    image = face_recognition.load_image_file(img)
-    face_locations = face_recognition.face_locations(image)
-
-    return face_locations
