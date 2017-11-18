6a2d92da41617cfcede73242b42153e923e6d982
igor <bujhm1313@gmail.com>
2017-11-18 20:13:55 +0200

    [IMP] Add img rendering

--- flask_server/static/js/find_face.js
+++ flask_server/static/js/find_face.js
@@ -2,7 +2,7 @@ window.onload = function () {
     var example = document.getElementById("coordinates");
     ctx = example.getContext('2d');
     pic = new Image();
-    pic.src = {{ img }}
+    pic.src = img_url;
     pic.onload = function () {
         ctx.drawImage(pic, 0, 0);
     };
