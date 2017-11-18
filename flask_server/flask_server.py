cd84c2daceeb278e66306fad7712c670c80c43e2
igor <bujhm1313@gmail.com>
2017-11-18 20:09:13 +0200

    [IMP] Add template rendering

--- flask_server/flask_server.py
+++ flask_server/flask_server.py
@@ -1,4 +1,8 @@
-from flask import Flask
+import requests
+import io
+from flask import Flask, render_template
+
+from face_recognition_utils import utils
 
 app = Flask(__name__)
 
@@ -10,7 +14,13 @@ def hello_world():
 
 @app.route('/find_face')
 def find_face():
-    return 'YOUR FACE'
+    img_url = "http://localhost:8000/igor-1.jpg"
+
+    img = io.BytesIO(requests.get(img_url).content)
+    face_locations = utils.get_face_coordinates(img)
+    return render_template('find_face_template.html',
+                           img=img_url,
+                           coordinates=face_locations)
 
 
 if __name__ == '__main__':
